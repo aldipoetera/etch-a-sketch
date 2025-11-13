d11395f1a17382abe7dc39e6b986040a130c977f
aldipoetera <192598636+aldipoetera@users.noreply.github.com>
2025-11-13 10:43:46 +0700

    Add web, styling and JS prototype

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const canvasGridDoc = document.querySelector(".canvas");
+const canvasSizeDoc = document.getElementById("canvasSizeInput")
+const colorRGBDoc = document.getElementById("colorRGB")
+const colorAlphaDoc = document.getElementById("colorAlpha")
+const resetColorDoc = document.getElementById("resetColor")
+const colorValidationDoc = document.getElementById("colorValidation")
