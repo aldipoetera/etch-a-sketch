6c9fd400527103abb6688cb60cac492de8a31823
aldipoetera <192598636+aldipoetera@users.noreply.github.com>
2025-11-13 18:00:03 +0700

    Add canvas drawing interactivity
    Added clear canvas and extra properties to grid with minor fixes

--- javascript.js
+++ javascript.js
@@ -6,10 +6,19 @@ const colorPickerDoc = document.querySelector(".colorPick")
 const resetColorDoc = document.getElementById("resetColor")
 const colorValidationDoc = document.getElementById("colorValidation")
 
+let isMouseDown = false;
+document.addEventListener("mousedown",  () => {
+  isMouseDown = true;
+})
+document.addEventListener("mouseup",  () => {
+  isMouseDown = false;
+})
+
 let currRGB = [0,0,0];
 let currAlpha = 1;
 colorRGBDoc.value = "#000000"
 colorAlphaDoc.value = 1
+addCanvasGrid(canvasSizeDoc.value)
 updateColorValidation()
 
 function createCanvasGrid (size) {
@@ -17,12 +26,14 @@ function createCanvasGrid (size) {
     const canvasColumn = document.createElement('div')
     canvasColumn.classList.add(`column`)
     canvasColumn.id = `x-${index}`
+    canvasColumn.draggable = false
     return canvasColumn
   }).map((item) => {
     const createCanvasRow = (jndex) => {
       const canvasGrid = document.createElement('div')
       canvasGrid.classList.add(`grid`)
       canvasGrid.id = `y-${jndex}`
+      canvasGrid.draggable = false
       return canvasGrid
     }
     for (let j = 0; j < size; j++) {
@@ -38,11 +49,29 @@ function addCanvasGrid (size) {
   createCanvasGrid(size).forEach((item) => {
     canvasGridDoc.appendChild(item)
   });
-}
 
-canvasSizeDoc.addEventListener("change", (e) => {
-  addCanvasGrid(e.target.value)
-})
+  document.querySelectorAll('.grid').forEach((item) => {
+
+    // // TOP's project requirements; I am not confined to such restrictions.
+    // item.addEventListener("mouseenter", () => {
+    //   item.style.backgroundColor = `rgba(${currRGB.join(', ')}, ${currAlpha})`
+    // })
+
+    item.addEventListener("mouseenter", () => {
+      if (isMouseDown === true) {
+        item.style.backgroundColor = `rgba(${currRGB.join(', ')}, ${currAlpha})`
+      }
+    })
+    item.addEventListener("mousedown", () => {
+      item.style.backgroundColor = `rgba(${currRGB.join(', ')}, ${currAlpha})`
+    })
+
+    // Prevents pesky dragging when you're drawing
+    item.addEventListener('dragstart', (e) => {
+        e.preventDefault();
+    });
+  });
+}
 
 function convertRGBText (sRGBValue) {
   const parseColorValue = (colorStr, pos) => parseInt(colorStr.slice(pos, pos+2), 16)
@@ -56,12 +85,15 @@ function updateColorValidation () {
   colorValidationDoc.textContent = `${currRGB.join(", ")} \\ ${currAlpha}`
 }
 
+canvasSizeDoc.addEventListener("change", (e) => {
+  addCanvasGrid(e.target.value)
+})
+
 colorPickerDoc.addEventListener("change", (e) => {
   if (e.target.id === "colorRGB") {
     currRGB = convertRGBText(e.target.value);
     updateColorValidation()
   } else if (e.target.id === "colorAlpha") {
-    console.log(e.target.value)
     currAlpha = +e.target.value;
     updateColorValidation()
   }
@@ -75,4 +107,10 @@ colorPickerDoc.addEventListener("click", (e) => {
     currAlpha = 1
     updateColorValidation()
   }
+})
+
+document.querySelector("#clearCanvas").addEventListener("click", () => {
+  document.querySelectorAll(".grid").forEach((item) => {
+    item.style.backgroundColor = ''
+  })
 })
