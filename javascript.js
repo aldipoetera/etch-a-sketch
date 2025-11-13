2f6bd56ee14a5d583be733e16460a90b37f1773f
aldipoetera <192598636+aldipoetera@users.noreply.github.com>
2025-11-13 16:03:59 +0700

    Finish color picking and validation

--- javascript.js
+++ javascript.js
@@ -2,11 +2,18 @@ const canvasGridDoc = document.querySelector(".canvas");
 const canvasSizeDoc = document.getElementById("canvasSizeInput")
 const colorRGBDoc = document.getElementById("colorRGB")
 const colorAlphaDoc = document.getElementById("colorAlpha")
+const colorPickerDoc = document.querySelector(".colorPick")
 const resetColorDoc = document.getElementById("resetColor")
 const colorValidationDoc = document.getElementById("colorValidation")
 
+let currRGB = [0,0,0];
+let currAlpha = 1;
+colorRGBDoc.value = "#000000"
+colorAlphaDoc.value = 1
+updateColorValidation()
+
 function createCanvasGrid (size) {
-  const canvasColumnsDocArr = Array(+size).fill(0).map((_, index, arr) => {
+  const canvasColumnsDocArr = Array(+size).fill(0).map((_, index) => {
     const canvasColumn = document.createElement('div')
     canvasColumn.classList.add(`column`)
     canvasColumn.id = `x-${index}`
@@ -35,4 +42,37 @@ function addCanvasGrid (size) {
 
 canvasSizeDoc.addEventListener("change", (e) => {
   addCanvasGrid(e.target.value)
+})
+
+function convertRGBText (sRGBValue) {
+  const parseColorValue = (colorStr, pos) => parseInt(colorStr.slice(pos, pos+2), 16)
+  const rValue = parseColorValue(sRGBValue, 1)
+  const gValue = parseColorValue(sRGBValue, 3)
+  const bValue = parseColorValue(sRGBValue, 5)
+  return [rValue, gValue, bValue]
+}
+
+function updateColorValidation () {
+  colorValidationDoc.textContent = `${currRGB.join(", ")} \\ ${currAlpha}`
+}
+
+colorPickerDoc.addEventListener("change", (e) => {
+  if (e.target.id === "colorRGB") {
+    currRGB = convertRGBText(e.target.value);
+    updateColorValidation()
+  } else if (e.target.id === "colorAlpha") {
+    console.log(e.target.value)
+    currAlpha = +e.target.value;
+    updateColorValidation()
+  }
+})
+
+colorPickerDoc.addEventListener("click", (e) => {
+  if (e.target.id === "resetColor") {
+    colorRGBDoc.value = "#000000"
+    colorAlphaDoc.value = 1
+    currRGB = [0, 0, 0]
+    currAlpha = 1
+    updateColorValidation()
+  }
 })
