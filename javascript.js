0041f310e81dfcc88fb2f9527e1bd0f66b6412ed
aldipoetera <192598636+aldipoetera@users.noreply.github.com>
2025-11-13 12:53:43 +0700

    Create canvas grid

--- javascript.js
+++ javascript.js
@@ -4,3 +4,32 @@ const colorRGBDoc = document.getElementById("colorRGB")
 const colorAlphaDoc = document.getElementById("colorAlpha")
 const resetColorDoc = document.getElementById("resetColor")
 const colorValidationDoc = document.getElementById("colorValidation")
+
+function createCanvasGrid (size) {
+  const canvasColumnsDocArr = Array(size).fill(0).map((_, index) => {
+    const canvasColumn = document.createElement('div')
+    canvasColumn.classList.add(`column`)
+    canvasColumn.id = `x-${index}`
+    return canvasColumn
+  }).map((item) => {
+    const createCanvasRow = (jndex) => {
+      const canvasGrid = document.createElement('div')
+      canvasGrid.classList.add(`grid`)
+      canvasGrid.id = `y-${jndex}`
+      return canvasGrid
+    }
+    for (let j = 0; j < size; j++) {
+      item.appendChild(createCanvasRow(j));
+    }
+    return item
+  })
+
+  return canvasColumnsDocArr
+}
+
+function addCanvasGrid (size) {
+  canvasGridDoc.replaceChildren();
+  createCanvasGrid(size).forEach((item) => {
+    canvasGridDoc.appendChild(item)
+  });
+}
